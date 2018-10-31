0d6beaf89422933c419b332964acacfa81c6f726
Jeremy Puchta <jeremypuchta@outlook.com>
2018-10-31 18:06:54 +0100

    Modified Code to fulfill Pep8 standard

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_docker() -> str:
     return "Hello Docker!"
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
