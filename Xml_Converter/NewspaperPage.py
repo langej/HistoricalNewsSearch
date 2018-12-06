d3c0fd18920dbd4561d4d87bdf44767adc22a57a
Beussel <al-ali_ali@web.de>
2018-12-06 19:49:34 +0100

    XML_Converter hinzugefuegt

---
+++ Xml_Converter/NewspaperPage.py
@@ -0,0 +1,15 @@
+
+class NewspaperPage(object):
+
+    def __init__(self, year=0, month=0, day=0, newspaper_number=0, page_number=0,
+                 edition=0, issue=0, text=''):
+        self.Year = year
+        self.Month = month
+        self.Day = day
+        self.Newspaper_Number = newspaper_number
+        self.Page_Number = page_number
+        self.Edition = edition
+        self.Issue = issue
+        self.Text = text
+
+
