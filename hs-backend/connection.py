e7f1efeacd345f7274d6bc8f38f96410f01ae78f
Jeremy Puchta <jeremypuchta@outlook.com>
2018-12-12 16:23:31 +0100

    Add helper function to reset the ES index.

--- hs-backend/connection.py
+++ hs-backend/connection.py
@@ -18,3 +18,17 @@ def connect_elasticsearch():
             is_connected = True
         except Exception as err:
             print('Connection failed, Retrying...', err)
+
+
+#TODO: Implement a 'mapping' for the newspaper data schema
+def put_newspaper_mapping():
+    pass
+
+
+# Helper function to reset the Elasticsearch index
+def reset_index():
+    if es.indices.exists({index}):
+        es.indices.delete({index})
+
+    es.indices.create({index})
+    put_newspaper_mapping()
