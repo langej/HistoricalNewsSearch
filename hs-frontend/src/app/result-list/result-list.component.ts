cbd8fe9f2cde3721b58b6f4d0c4d71c6cc92ca50
Jonathan Lange <jonathanlange@web.de>
2019-01-16 22:44:51 +0100

    fixed problem with http-observable

--- hs-frontend/src/app/result-list/result-list.component.ts
+++ hs-frontend/src/app/result-list/result-list.component.ts
@@ -6,24 +6,35 @@ import {Newspaper} from "../shared/Newspaper";
 @Component({
   selector: 'hs-result-list',
   templateUrl: './result-list.component.html',
-  styleUrls: ['./result-list.component.css']
+  styleUrls: ['./result-list.component.css'],
+  providers: [SearchService]
 })
 export class ResultListComponent implements OnInit {
 
   private newspapers: Newspaper[];
 
-  //TODO: Edit so that query is still visible in searchbar
+  // TODO: Edit so that query is still visible in searchbar
 
   constructor(private router: Router, private searchService: SearchService) {
-    if (this.router.url !== '/search') {
-      let url = this.cleanUrl();
-      this.newspapers = this.searchService.getSearchResults(url);
-    }
+    // if (this.router.url !== '/search') {
+    //   const url = this.cleanUrl();
+    //   // this.newspapers = this.searchService.getSearchResults(url);
+    // }
 
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
   }
 
   ngOnInit() {
+    this.newspapers = new Array<Newspaper>();
+    if (this.router.url !== '/search') {
+      const url = this.cleanUrl();
+      this.searchService.getSearchResults(url).then( (results) => {
+        results.map( (data) => {
+          this.newspapers.push(data);
+        });
+        console.log(this.newspapers);
+      });
+    }
   }
 
   private cleanUrl() {
--- hs-frontend/src/app/shared/search.service.ts
+++ hs-frontend/src/app/shared/search.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Newspaper } from "./Newspaper";
 import {Observable} from "rxjs";
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
@@ -12,21 +13,19 @@ export class SearchService {
 
   constructor(private http: HttpClient) { }
 
-  getSearchResults(query: string): Array<Newspaper> {
-    const results = Array<Newspaper>();
-
-    this.http.get(this.api + query).subscribe( (data: any) => {
-      if (data.Items !== 0) {
-        Object.entries(data).forEach(([key, value]) => {
-          const content = <any> value;
-          const res: Newspaper = {
-            id: content._id,
-            source: content._source
-          };
-          results.push(res);
-        });
-      }
-    });
-    return results;
+  async getSearchResults(query: string) {
+    const response = await axios.get(this.api + query);
+    const newspapers = new Array<Newspaper>();
+    if (typeof(response.data) !== 'string') {
+      Object.entries(response.data).forEach(([key, value]) => {
+        const content = <any> value;
+        const res: Newspaper = {
+          id: content._id,
+          source: content._source
+        };
+        newspapers.push(res);
+      });
+    }
+    return newspapers;
   }
 }
