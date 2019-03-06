187df20f3200a74c1cad11ed095a2a49ff4a2c02
Jonathan Lange <jonathanlange@web.de>
2019-03-06 15:19:21 +0100

    add texthighlighting in detailview

--- hs-frontend/src/app/newspaper-detail/newspaper-detail.component.ts
+++ hs-frontend/src/app/newspaper-detail/newspaper-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { SearchService } from "../shared/search.service";
 import { ActivatedRoute, Router } from "@angular/router";
 import { Newspaper } from "../shared/Newspaper";
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'hs-newspaper-detail',
@@ -13,10 +14,15 @@ export class NewspaperDetailComponent implements OnInit {
 
   newspaper: Newspaper;
 
+  highlightedText: SafeHtml;
+
+  query: string;
+
   constructor (
     private ss: SearchService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private sanitizer: DomSanitizer
   ) {}
 
   ngOnInit() {
@@ -24,9 +30,22 @@ export class NewspaperDetailComponent implements OnInit {
     this.newspaper = new Newspaper();
     this.newspaper.id = params.id;
     this.newspaper.source = JSON.parse(localStorage.getItem(params.id));
+    this.query = decodeURI(localStorage.getItem('query'));
+    this.markQueryWords();
   }
 
   navigateBack() {
     this.router.navigate(['/search'], { queryParams: { q: decodeURI(localStorage.getItem('query'))}});
+  }
+
+  markQueryWords() {
+    let queryWords = this.query.split(" ");
+
+    let text: string = this.newspaper.source.Text;
+
+    queryWords.map( (value) => {
+      text = text.replace(new RegExp(`${value}`, 'gi'), `<span class="marked">${value}</span>`);
+    })
+    this.highlightedText = this.sanitizer.bypassSecurityTrustHtml(text);
   }
 }
