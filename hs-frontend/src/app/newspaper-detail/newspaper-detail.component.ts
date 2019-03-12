adc0478ea4e1486e2aa6ee2160348259ab973461
Jonathan Lange <jonathanlange@web.de>
2019-03-12 10:31:07 +0100

    add images to newspaper-detail view

--- hs-frontend/src/app/newspaper-detail/newspaper-detail.component.ts
+++ hs-frontend/src/app/newspaper-detail/newspaper-detail.component.ts
@@ -2,7 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { SearchService } from "../shared/search.service";
 import { ActivatedRoute, Router } from "@angular/router";
 import { Newspaper } from "../shared/Newspaper";
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { DomSanitizer, SafeHtml, SafeUrl } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'hs-newspaper-detail',
@@ -18,6 +19,8 @@ export class NewspaperDetailComponent implements OnInit {
 
   query: string;
 
+  image: SafeUrl;
+
   constructor (
     private ss: SearchService,
     private route: ActivatedRoute,
@@ -32,6 +35,7 @@ export class NewspaperDetailComponent implements OnInit {
     this.newspaper.source = JSON.parse(localStorage.getItem(params.id));
     this.query = decodeURI(localStorage.getItem('query'));
     this.markQueryWords();
+    this.loadImage();
   }
 
   navigateBack() {
@@ -41,11 +45,30 @@ export class NewspaperDetailComponent implements OnInit {
   markQueryWords() {
     let queryWords = this.query.split(" ");
 
-    let text: string = this.newspaper.source.Text;
+    let text = ''
+
+    this.newspaper.source.Text.map(
+      elem => {
+        elem.map(
+          elem => {
+            let tmp = elem.join(' ')
+            text = text.concat(tmp)
+            text = text.concat('\n')
+          }
+        )
+        text = text.concat('\n')
+      }
+    )
+    console.log(this.newspaper.source);
 
     queryWords.map( (value) => {
       text = text.replace(new RegExp(` ${value} `, 'gi'), ` <span style="background: yellow;">${value}</span> `);
     })
     this.highlightedText = this.sanitizer.bypassSecurityTrustHtml(text);
+  }
+
+  loadImage() {
+    let d = this.newspaper.source
+    this.image = this.sanitizer.bypassSecurityTrustUrl(`http://localhost:5000/image/${d.Year}_${d.Day}_${d.Month}_${d.NewspaperNumber}_${d.Edition}_${d.PageNumber}`);
   }
 }
--- hs-frontend/src/app/searchbar/searchbar.component.ts
+++ hs-frontend/src/app/searchbar/searchbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {SearchService} from "../shared/search.service";
+import Axios from 'axios';
 
 @Component({
   selector: 'hs-searchbar',
@@ -11,6 +12,8 @@ export class SearchbarComponent implements OnInit {
 
   query: string;
 
+  recommendations: string[];
+
   constructor(private router: Router, private activeRoute: ActivatedRoute) {
     this.activeRoute.queryParams.subscribe(params => {
       this.query = params["q"];
@@ -18,9 +21,25 @@ export class SearchbarComponent implements OnInit {
   }
 
   ngOnInit() {
+    let input = document.getElementById('search-input');
+    input.addEventListener("input", (event: any) => {
+      Axios.get(`http://localhost:5000/recommendation/${event.target.value}`).then(
+        response => {
+          this.recommendations = [];
+          response.data.map(
+            elem => {
+              this.recommendations.push(elem[0]);
+            }
+          );
+          console.log(this.recommendations);
+        }
+      )
+    })
   }
 
   navigate() {
     this.router.navigate(["/search"], { queryParams: { q: this.query }});
   }
+
+
 }
